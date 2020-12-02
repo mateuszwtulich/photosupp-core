da79f2e1c1c6d5d034975b0e8bb57a120431ce6e
WTULICH <mateusz.wtulich@capgemini.com>
2020-12-02 19:29:22 +0100

    jar spring

--- src/main/java/com/wtulich/photosupp/general/config/CorsConfig.java
+++ src/main/java/com/wtulich/photosupp/general/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").
-                        allowedOrigins("http://localhost:4200").
+                        allowedOrigins("https://photosupp-client.herokuapp.com").
                         allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials( true )
